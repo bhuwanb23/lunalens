99758edf41ab5c39e7333034e454d3d4abba7ede
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-04 21:13:10 +0530

    style(login): remove unused AuthTabs, RightPanel, and Footer components from index.js

--- frontend/website/src/pages/login/components/index.js
+++ frontend/website/src/pages/login/components/index.js
@@ -1,6 +1,3 @@
 export { default as LoginCard } from './LoginCard';
 export { default as LoginForm } from './LoginForm';
-export { default as AuthTabs } from './AuthTabs';
 export { default as SocialLogin } from './SocialLogin';
-export { default as RightPanel } from './RightPanel';
-export { default as Footer } from './Footer';
