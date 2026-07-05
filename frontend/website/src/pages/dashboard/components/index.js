f4a0b7985d795ea128cbfc3db3b103dbe451b7e9
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-05 08:30:14 +0530

    feat: update component barrel exports for new dashboard

--- frontend/website/src/pages/dashboard/components/index.js
+++ frontend/website/src/pages/dashboard/components/index.js
@@ -1,6 +1,6 @@
-export { default as StatsCard } from './StatsCard';
-export { default as LunarAnalysis } from './LunarAnalysis';
-export { default as AlertsPanel } from './AlertsPanel';
-export { default as QuickActions } from './QuickActions';
-export { default as RecentScans } from './RecentScans';
-export { default as Footer } from './Footer'; 
+export { default as ProfileCard } from './ProfileCard';
+export { default as GradientStatCard } from './GradientStatCard';
+export { default as TrackersCard } from './TrackersCard';
+export { default as TrendsChart } from './TrendsChart';
+export { default as SessionsPanel } from './SessionsPanel';
+export { default as ProgressPanel } from './ProgressPanel';
